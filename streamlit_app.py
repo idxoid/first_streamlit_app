69fd8cd734d5a73d8502229c8bc2a3363394897c
idxoid <idxoid@gmail.com>
2022-10-28 18:27:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as sl
 
-sl.title('My parents New Happy Diner')
+sl.title('Breakfast Favorites')
 sl.header('Breakfast Menu')
 sl.text('Omega 3 & Blubery Oatmeal')
 sl.text('Kale, Spinach & Rocket Smoothie')
 sl.text('Hard-Boiled Free-Range Egg')
+sl.text('Avokado toast')
 
+
+sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
