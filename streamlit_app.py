b1ca9ef5b9abe71d090d509286e19f3287abc713
idxoid <idxoid@gmail.com>
2022-10-28 18:34:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,5 @@ sl.text('🥑🍞Avokado toast')
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list=pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-sl.multiselect('Chjose fruits please',list(my_fruit_list))
+sl.multiselect('Chjose fruits please',list(my_fruit_list.index))
 sl.dataframe(my_fruit_list)
