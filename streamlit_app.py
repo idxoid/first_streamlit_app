6e8c519daa4020a282f4ae476cb3f260275b8ff9
idxoid <idxoid@gmail.com>
2022-10-28 18:43:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list=pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = sl.multiselect('Chjose fruits please',list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruits_list.loc[fruits_selected]
+fruit_to_show = my_fruit_list.loc[fruits_selected]
 
-sl.dataframe(fruits_to_show)
+sl.dataframe(fruit_to_show)
